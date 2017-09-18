02f21bed43f533cd7174993bd06eadb2746351f6
Murselov Mirmursel <mmirmursal.p302@code.edu.az>
2017-09-18 06:05:39 +0400

    Add template Asp.net(MVC) --First--Try--

---
+++ Colossious_Asp.net_MVC/Colossious_Asp.net_MVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Colossious_Asp.net_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Full_Width()
+        {
+            return View();
+        }
+
+        public ActionResult Sidebar_Left()
+        {
+            return View();
+        }
+        public ActionResult Sidebar_Right()
+        {
+            return View();
+        }
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+        public ActionResult Basic_Grid()
+        {
+            return View();
+        }
+    }
+}
---
+++ Colossious_Asp.net_MVC/Colossious_Asp.net_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Colossious_Asp.net_MVC.MvcApplication" Language="C#" %>
